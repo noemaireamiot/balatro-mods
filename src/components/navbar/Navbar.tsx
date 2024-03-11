7416114e27f3efe2dbcd67f4068e4c35983a55e2
Noe Maire-Amiot <noe.maireamiot@gmail.com>
2024-03-11 22:39:15 +0100

    feat: Add connexion

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,52 +1,17 @@
-import { Navigate, useLocation, useNavigate } from 'react-router-dom'
 import netlifyIdentity from 'netlify-identity-widget'
-import { useState } from 'react'
 import { Button } from '../Button/Button'
 
 const Navbar = () => {
-    const navigate = useNavigate()
-    const location = useLocation()
-    const [redirectToReferrer, setRedirectToReferrer] = useState<boolean>(false)
-
-    const netlifyAuth = {
-        isAuthenticated: false,
-        user: null,
-        authenticate(callback: (user: any) => void) {
-            this.isAuthenticated = true
-            netlifyIdentity.open()
-            netlifyIdentity.on('login', (user) => {
-                localStorage.setItem('user', JSON.stringify(user))
-                callback(user)
-            })
-        },
-        signout(callback: () => void) {
-            this.isAuthenticated = false
-            netlifyIdentity.logout()
-            netlifyIdentity.on('logout', () => {
-                this.user = null
-                callback()
-            })
-        },
-    }
-
-    const login = () => {
-        netlifyAuth.authenticate(() => {
-            setRedirectToReferrer(true)
-        })
-    }
-
-    const { from } = location.state || { from: { pathname: '/' } }
-
-    if (redirectToReferrer) return <Navigate to={from} replace={true} />
-
     return (
         <nav className="flex justify-end m-4">
-            {netlifyAuth.isAuthenticated ? (
+            {netlifyIdentity.currentUser() ? (
                 <p>
-                    Welcome !{' '}
                     <Button
                         onClick={() => {
-                            netlifyAuth.signout(() => navigate('/'))
+                            netlifyIdentity.logout()
+                            netlifyIdentity.on('logout', () => {
+                                window.location.href = '/'
+                            })
                         }}
                         size="small"
                         background="red"
@@ -55,7 +20,13 @@ const Navbar = () => {
                 </p>
             ) : (
                 <Button
-                    onClick={login}
+                    onClick={() => {
+                        netlifyIdentity.open()
+                        netlifyIdentity.on('login', () => {
+                            netlifyIdentity.close()
+                            window.location.reload()
+                        })
+                    }}
                     size="small"
                     background="green"
                     label="Log in"
